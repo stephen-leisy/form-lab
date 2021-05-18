30a6ce9c8f0904181f1b6b2e4beb4788f90b95b4
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-05-17 21:19:03 -0700

    trying to fix something CI is mad about that nothing in the app is mad about.

--- src/services/newsApi.jsx
+++ src/services/newsApi.jsx
@@ -5,7 +5,7 @@ export const getAllNews = async () => {
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
   );
   const { articles } = await news.json();
-  //   const articles = json.articles;
+  console.log('api:', articles);
 
   return articles.map(({ author, title, description, url }) => ({
     author,
@@ -19,12 +19,12 @@ export const searchNews = async (query) => {
   const search = await fetch(
     `https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.NEWS_API_KEY}`
   );
-  const { articles } = await search.json();
-
-  return articles.map(({ author, title, description, url }) => ({
-    author,
-    title,
-    description,
-    url,
+  const json = await search.json();
+  console.log('search:', json.articles);
+  return json.articles.map((article) => ({
+    author: article.author,
+    title: article.title,
+    description: article.description,
+    url: article.url,
   }));
 };
